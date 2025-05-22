f64319bd3000825d166bff8029e41c80860d5e29
jean <jeang51@nycstudents.net>
2025-05-22 09:09:11 -0400

    123

--- app.py
+++ app.py
@@ -1,49 +1,30 @@
+
+
+
+    
+
+    
+    
 from flask import Flask, render_template
 import requests
 
 app = Flask(__name__)
 
-response = requests.get("https://www.fruityvice.com/api/fruit/all")
-data = response.json()
-
-
-fruit_list = []
-
-for fruit in fruit_list:
-   
-    
-    fruit_list.append({
-        'name': fruit['name'],
-        'id': id,
-        
-    })
-    print(fruit_list)
 @app.route("/")
 def index():
-    # Get the leagues from the API
-    fruit = requests.get("https://www.fruityvice.com/api/fruit/all")
-    data = response.json()
-    fruit_list = data['data']  # The correct key is 'data', not 'results'
-    print(data)
+    # Get fruit data from API
+    response = requests.get("https://www.fruityvice.com/api/fruit/all")
+    fruit_data = response.json()  # This returns a list
 
-    music = []
-
-    for fruit in fruit_list:
-        url = fruit['slug']  # 'slug' usually contains a unique ID or path
-        id = url
-        
-        fruit.append({
-            'name': fruit['name'],
-            'id': id,
-            
+    # Create a list of fruits with desired fields
+    fruit_list = []
+    for fruit in fruit_data:
+        fruit_list.append({
+            'name': fruit.get('name'),
+            'id': fruit.get('id')
         })
 
-    return render_template("index.html", fruit=fruit)
+    return render_template("index.html", fruits=fruit_list)
 
-
-
-    
-
-    
-    
-    
+if __name__ == "__main__":
+    app.run(debug=True)
